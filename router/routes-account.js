c17f5b17078ebe37a7059ea259718ff175d5fa98
luqni <mluqnibaehaqi@gmail.com>
2018-12-21 21:53:50 +0700

    update modul account

--- router/routes-account.js
+++ router/routes-account.js
@@ -3,4 +3,7 @@ module.exports = function(app){
 
     app.route('/accounts').get(controller.accounts);
     app.route('/account/:id').get(controller.getAccountrById);
+    app.route('/account').post(controller.insertAccount);
+    app.route('/account').put(controller.updateAccount);
+    app.route('/account/:id').delete(controller.del);
 }
